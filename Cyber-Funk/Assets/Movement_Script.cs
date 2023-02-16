dc428c7756b875e4586b89579975157fd0441db1
Anton the Hobo <119599859+Anton-The-Hobo@users.noreply.github.com>
2023-02-16 18:01:34 +0100

    Onga Bonga

--- Cyber-Funk/Assets/Movement_Script.cs
+++ Cyber-Funk/Assets/Movement_Script.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class Movement_Script : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private float horizontal; //Variabel för input på x-axeln
+    private float vertical; //Variabel för input på y-axeln
+    public float MoveSpeed = 8; //Variabel för hur mycket spelaren ska röra på sig
 
-    // Update is called once per frame
+    private Vector2 Direction;
+
+    public Rigidbody2D rb; //Så att spelet vet vilken rigidbody2D den ska använda
+
     void Update()
     {
-        
+        ControllInputs();
+    }
+
+    void FixedUpdate()
+    {
+        Move();    
+    }
+
+    void ControllInputs()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+        Direction = new Vector2(moveX, moveY).normalized;
+    }
+
+    void Move()
+    {
+        rb.velocity = new Vector2(Direction.x * MoveSpeed, Direction.y * MoveSpeed);
     }
 }
