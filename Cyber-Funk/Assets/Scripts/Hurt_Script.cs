5c5daa45d6626a709601dade0ce2f210e3a81229
AshtonsHappyPlace <ashton.andreassonlilja@elev.ga.lbs.se>
2023-03-29 15:55:23 +0200

    idk

--- Cyber-Funk/Assets/Scripts/Hurt_Script.cs
+++ Cyber-Funk/Assets/Scripts/Hurt_Script.cs
@@ -7,13 +7,13 @@ public class Hurt_Script : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D enemy) //Gör så att saker kan hända vid kollision
     {
-        if (enemy.gameObject.GetComponent<Movement_Script>().health <= 0) //om gameObject med taggen enemy health från scriptet movement är like med eller mindre än 0...
+        if (enemy.gameObject.GetComponent<PlayerHealth>().playerHealth <= 0) //om gameObject med taggen enemy health från scriptet movement är like med eller mindre än 0...
         {
             Destroy(enemy.gameObject); //förstö gameObjectet med taggen enemy
         }
         else //i andra fall...
         {
-            enemy.gameObject.GetComponent<Movement_Script>().health--; //ta bort en health point
+            enemy.gameObject.GetComponent<PlayerHealth>().playerHealth--; //ta bort en health point
         }
     }
 }
