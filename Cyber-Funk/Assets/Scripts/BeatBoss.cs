9d57ef6dfe31bee136e5e285e8020fd1769af033
Anton-The-Hobo <119599859+Anton-The-Hobo@users.noreply.github.com>
2023-04-26 14:55:00 +0200

    iuy

--- Cyber-Funk/Assets/Scripts/BeatBoss.cs
+++ Cyber-Funk/Assets/Scripts/BeatBoss.cs
@@ -2,9 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyDeath : MonoBehaviour
+public class BeatBoss : MonoBehaviour
 {
     public float health;
+    public GameObject go;
 
 
     private void OnTriggerEnter2D(Collider2D collision) //Gör så att saker kan hända vid kollision
@@ -16,9 +17,10 @@ public class EnemyDeath : MonoBehaviour
 
         if (collision.gameObject.tag == "Pellet" && health <= 0) //Om någon med EnemyDeath scriptet är i kontakt med något med tagen "pellet"...
         {
-           Destroy(gameObject);
+            GameObject newObject = Instantiate(go, new Vector2(10.77f, 8.8f), Quaternion.identity);
+            Destroy(gameObject);
         }
     }
 
-   
+
 }
